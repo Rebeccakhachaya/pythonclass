83673ce8c228833656e0c6eab9df6095db9e87bd
rebeccakhachaya <rebeccakhachaya@gmail.com>
2021-06-17 10:47:14 +0300

    worktest

---
+++ pythontest.py
@@ -0,0 +1,36 @@
+x=[100,110,120,130,140,150]
+print(x)
+t=x
+for i in t :
+    t=5
+    print(i*5)
+
+
+def divisible_by_three(n):
+    n=100
+    i=range(1,100)
+    for x in i:
+         if x %3==0:
+             print(i)
+
+def flattens():
+    x=[[1,2],[3,4],[5,6]]
+    print(x)
+
+def smallest():
+      x=[1,3,5,7,9]
+      x.sort()
+      print(x)
+      smallest()
+x=["a","b","a","e","d","b","c","e","f","g",]
+print(x)
+x.remove
+print(x)
+def student(**kwargs):
+  student1={"age":19,"name":"jnae",}
+  student2={"age":20,"name":"macy"}
+  student3={"age":24,"name":"laura"}
+  return f"hello${name} "
+
+
+    
